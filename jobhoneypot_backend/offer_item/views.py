70b7018871080ee6e031eee501865f390be5695c
Daniel Garrapucho <daniel.garrapucho@est.fib.upc.edu>
2018-10-20 21:32:06 +0200

    Dummy receptor peticion

--- jobhoneypot_backend/offer_item/views.py
+++ jobhoneypot_backend/offer_item/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Offer
+from django.http import HttpResponse
+
 # Create your views here.
 
 def load_offers(request):
-    offer_list = Offer.objects.all()
+    return HttpResponse(request)
