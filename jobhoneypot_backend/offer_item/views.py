6a2a5de0e50356c8dfc2bb3e23b39569d4a54e08
Daniel Garrapucho <daniel.garrapucho@est.fib.upc.edu>
2018-10-20 22:52:31 +0200

    error 403 corregido

--- jobhoneypot_backend/offer_item/views.py
+++ jobhoneypot_backend/offer_item/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 from .models import Offer
 from django.http import HttpResponse
-
+import matchFields
+import jobhoneypot_backend.apirequest
 # Create your views here.
-
+@csrf_exempt
 def load_offers(request):
+    #return HttpResponse("Hello World")
+
+
     return HttpResponse(request)
+
