8f9c488e5dcbaed99183bfe88fbb41bff49a2721
Daniel Garrapucho <daniel.garrapucho@est.fib.upc.edu>
2018-10-20 15:30:33 +0200

    Initialize Offer Models

--- jobhoneypot_backend/offer_item/models.py
+++ jobhoneypot_backend/offer_item/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Offer(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=200,default=None)
+    link = models.CharField(max_length=200,default=None)
+    category = models.CharField(max_length=200,default=None)
+    contractType = models.CharField(max_length=200,default=None)
+    salaryMax = models.CharField(max_length=200,default=None)
+
+class Category(models.Model):
+    cat_id = models.IntegerField(primary_key=True)
+    key_name = models.CharField(max_length=200,default=None)
+    val_name = models.CharField(max_length=200,default=None)
